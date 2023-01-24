666aabed3e1af0322a541c9d28fff8c6cd6b257a
vgaidukov <vgaidukov@gmail.com>
2023-01-24 13:40:19 +0300

    feat: footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,33 @@ function Footer() {
 
   return (
     <footer className="footer">
-      FOOTER
+      <div className="footer__container">
+
+        <div className="footer__bottom">
+          <p className="footer__copyright">&#169; 2023</p>
+          <p className="footer__text">
+            Designed & developed by Vadim Gaidukov
+          </p>
+          <div className="footer__links">
+            <a
+              className="link footer__link"
+              href="https://movies-vg.nomoredomains.icu/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              My Page
+            </a>
+            <a
+              className="link footer__link"
+              href="https://github.com/vgaidukov"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
